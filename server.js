eb202cd136d0f5910b4e328eea79e2d19841a011
Dane Kim <mr.dane@danes-mbp.lan>
2022-05-28 15:13:58 -0400

    setting up ejs layout and view engine

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(layouts);
 
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
