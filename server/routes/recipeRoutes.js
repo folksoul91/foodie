5ab81eb9176d7f4294e51fab2092ea7dce49ab74
Dane Kim <mr.dane@danes-mbp.lan>
2022-05-29 12:46:12 -0400

    adding descriptions

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -1,7 +1,9 @@
+// Dependencies
 const express = require("express");
 const router = express.Router();
 const recipeController = require("../controllers/recipeController");
 
+// Index
 router.get("/", recipeController.homepage);
 
 module.exports = router;
