24618f12e37d99760d1d40da7e0a0758ed3c92b3
Dane Kim <mr.dane@danes-mbp.lan>
2022-05-29 11:19:15 -0400

    setting the route dependency from recipeRoute

--- server.js
+++ server.js
@@ -10,8 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(layouts);
 
+// setting the layout
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
+
+// setting the route dependency
+const routes = require("./server/routes/recipeRoutes.js");
+app.use("/", routes);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
