da7df2e982f990de03dc76bdaf6d1a4f923bdf95
Dane Kim <mr.dane@danes-mbp.lan>
2022-05-31 14:09:46 -0400

    adding routes to recipe id, category id, and post route

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -5,6 +5,9 @@ const recipeController = require("../controllers/recipeController");
 
 // Index
 router.get("/", recipeController.homepage);
+router.get("/recipe/:id", recipeController.navigateRecipe);
 router.get("/categories", recipeController.navigateCategories);
+router.get("/categories/:id", recipeController.navigateCategoriesById);
+router.post("/search", recipeController.searchForRecipe);
 
 module.exports = router;
