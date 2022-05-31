fba57a8183d3bd3df9caea1e48ae9b4023238e09
Dane Kim <mr.dane@danes-mbp.lan>
2022-05-31 19:24:42 -0400

    mounting routes for trending page and submit-recipe page

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -3,11 +3,13 @@ const express = require("express");
 const router = express.Router();
 const recipeController = require("../controllers/recipeController");
 
-// Index
+// mounting routes
 router.get("/", recipeController.homepage);
 router.get("/recipe/:id", recipeController.navigateRecipe);
 router.get("/categories", recipeController.navigateCategories);
 router.get("/categories/:id", recipeController.navigateCategoriesById);
 router.post("/search", recipeController.searchForRecipe);
+router.get("/trending", recipeController.seeTrend);
+router.get("/submit-recipe", recipeController.submitRecipe);
 
 module.exports = router;
