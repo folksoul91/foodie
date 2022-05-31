4d3f8590b35023462f981287c7c3e527020f28d8
Dane Kim <mr.dane@danes-mbp.lan>
2022-05-31 14:11:24 -0400

    adding logics to homepage, creating category by id, recipe id, and search route

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,16 +1,46 @@
 require("../models/database");
 const Category = require("../models/category");
+const Recipe = require("../models/recipe");
 
 // Homepage
 exports.homepage = async (req, res) => {
   const limitNumber = 5;
   const categories = await Category.find({}).limit(limitNumber);
-  res.render("index", { title: "homepage", categories });
+  const trending = await Recipe.find({}).sort({ _id: -1 }).limit(limitNumber);
+  const american = await Recipe.find({ category: "American" }).limit(
+    limitNumber
+  );
+  const thai = await Recipe.find({ category: "Thai" }).limit(limitNumber);
+  const chinese = await Recipe.find({ category: "Chinese" }).limit(limitNumber);
+  const food = { trending, american, thai, chinese };
+  res.render("index", { title: "homepage", categories, food });
 };
 
 // Navigate Categories
 exports.navigateCategories = async (req, res) => {
-  const limitNumber = 10;
+  const limitNumber = 15;
   const categories = await Category.find({}).limit(limitNumber);
   res.render("categories.ejs", { title: "Categories", categories });
+};
+
+// Navigate Categories by Id
+exports.navigateCategoriesById = async (req, res) => {
+  let categoryId = req.params.id;
+  const limitNumber = 15;
+  const categoryById = await Recipe.find({ category: categoryId }).limit(
+    limitNumber
+  );
+  res.render("categories.ejs", { title: "Categories", categoryById });
+};
+
+// Recipe route/:id
+exports.navigateRecipe = async (req, res) => {
+  let recipeId = req.params.id;
+  const recipe = await Recipe.findById(recipeId);
+  res.render("recipe", { title: "Recipe", recipe });
+};
+
+// Search route
+exports.searchForRecipe = async (req, res) => {
+  res.render("search", { title: "Search" });
 };
